4dc6d5bcea76d0d016daf730c7ac1611ffd9a484
Paul Lam <paul@quantisan.com>
2018-07-26 13:59:39 +0900

    release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject motiva/sqs-utils "0.3.0-SNAPSHOT"
+(defproject motiva/sqs-utils "0.2.2"
   :description "Higher level SQS utilities for use in Motiva products"
   :url "https://github.com/Motiva-AI/sqs-utils"
   :license {:name "Proprietary" :url "https://motiva.ai"}
