0c9cb739594dd4b57bed971c23311b484c618e67
J Irving <j@lollyshouse.ca>
2019-04-03 15:41:49 -0400

    Update tests to cover :json and :transit

--- test/sqs_utils/core_test.clj
+++ test/sqs_utils/core_test.clj
@@ -25,169 +25,187 @@
   (f)
   (test-utils/delete-queue! (sqs-config) @test-queue-url))
 
-;; create and delete queue are async and the queue might not be available
-;; between tests if we do :each - take note
-(use-fixtures :each wrap-queue)
+;; (use-fixtures :each wrap-queue)
+
+(defmacro with-queue [& body]
+  `(wrap-queue (fn [] ~@body)))
 
 (deftest receive-one-test
-  (let [creds (sqs-config)]
-    (is (su/send-message creds @test-queue-url {:testing 3}))
-    (is (= {:testing 3} (su/receive-one! creds @test-queue-url)))))
+  (doseq [format [:json :transit]]
+    (with-queue
+      (let [creds (sqs-config)]
+        (is (su/send-message creds @test-queue-url {:testing 3} {:format format}))
+        (is (= {:testing 3} (su/receive-one! creds @test-queue-url)))))))
 
 (deftest send-and-receive-message-test
-  (let [c (chan)
-        creds (sqs-config)]
-    (is (su/send-message creds @test-queue-url {:testing 2}))
-    (let [kill-fn (su/receive-loop! creds @test-queue-url c)]
-      (is (fn? kill-fn))
-      (is (= {:testing 2}
-             (:message (<!! c))))
-      (is (su/send-message creds @test-queue-url {:testing 1}))
-      (is (= {:testing 1}
-             (:message (<!! c))))
-      (kill-fn))))
+  (doseq [format [:json :transit]]
+    (with-queue
+      (let [c (chan)
+            creds (sqs-config)]
+        (is (su/send-message creds @test-queue-url {:testing 2} {:format format}))
+        (let [kill-fn (su/receive-loop! creds @test-queue-url c)]
+          (is (fn? kill-fn))
+          (is (= {:testing 2}
+                 (:message (<!! c))))
+          (is (su/send-message creds @test-queue-url {:testing 1}))
+          (is (= {:testing 1}
+                 (:message (<!! c))))
+          (kill-fn))))))
 
 (deftest receipt-handle-present-when-not-auto-deleting
-  (let [creds (sqs-config)]
-    (testing "auto-delete is true"
-      (let [c       (chan)
-            stop-fn (su/receive-loop! creds @test-queue-url c {:auto-delete true})]
-        (is (su/send-message creds @test-queue-url {:testing 4}))
-        (let [result (<!! c)]
-          (is (= {:testing 4} (:message result)))
-          (is (not (contains? result :done-fn))))
-        (stop-fn)))
-    (testing "auto-delete is false"
-      (let [c       (chan)
-            stop-fn (su/receive-loop! creds @test-queue-url c {:auto-delete false})]
-        (is (su/send-message creds @test-queue-url {:testing 5}))
-        (let [result (<!! c)]
-          (is (= {:testing 5} (:message result)))
-          (is (contains? result :done-fn)))
-        (stop-fn)))))
+  (doseq [format [:json :transit]]
+    (with-queue
+      (let [creds (sqs-config)]
+        (testing "auto-delete is true"
+          (let [c       (chan)
+                stop-fn (su/receive-loop! creds @test-queue-url c {:auto-delete true})]
+            (is (su/send-message creds @test-queue-url {:testing 4} {:format format}))
+            (let [result (<!! c)]
+              (is (= {:testing 4} (:message result)))
+              (is (not (contains? result :done-fn))))
+            (stop-fn)))
+        (testing "auto-delete is false"
+          (let [c       (chan)
+                stop-fn (su/receive-loop! creds @test-queue-url c {:auto-delete false})]
+            (is (su/send-message creds @test-queue-url {:testing 5} {:format format}))
+            (let [result (<!! c)]
+              (is (= {:testing 5} (:message result)))
+              (is (contains? result :done-fn)))
+            (stop-fn)))))))
 
 (deftest client-acknowledgement-works
-  (let [creds (sqs-config)]
-    (testing "acknowledged messages don't get resent"
-      (let [c       (chan)
-            stop-fn (su/receive-loop! creds @test-queue-url c {:auto-delete false
-                                                               :visibility-timeout 5})]
-        (is (su/send-message creds @test-queue-url {:testing 6}))
-        (let [{:keys [message done-fn]} (<!! c)]
-          (is (= {:testing 6} message))
-          (is (some? done-fn))
-          ;; call the function, then wait for it not to show up :P
-          (done-fn)
-          (is (alt!!
-                c false
-                (async/timeout 10000) true)))
-        (stop-fn)))
-    (testing "unacknowledged messages get resent"
-      (let [c       (chan)
-            stop-fn (su/receive-loop! creds @test-queue-url c {:auto-delete false
-                                                               :visibility-timeout 5})]
-        (is (su/send-message creds @test-queue-url {:testing 7}))
-        (let [{:keys [message done-fn]} (<!! c)]
-          (is (= {:testing 7} message))
-          (is (some? done-fn))
-          ;; don't call it, wait for the next one
-          (wait-for
-            #(alt!!
-               c ([{:keys [message done-fn]} _]
-                  (= message {:testing 7}))
-               (async/timeout 500) false)
-            :timeout 30))
-        (stop-fn)))))
+  (doseq [format [:json :transit]]
+    (with-queue
+      (let [creds (sqs-config)]
+        (testing "acknowledged messages don't get resent"
+          (let [c       (chan)
+                stop-fn (su/receive-loop! creds @test-queue-url c {:auto-delete false
+                                                                   :visibility-timeout 5})]
+            (is (su/send-message creds @test-queue-url {:testing 6} {:format format}))
+            (let [{:keys [message done-fn]} (<!! c)]
+              (is (= {:testing 6} message))
+              (is (some? done-fn))
+              ;; call the function, then wait for it not to show up :P
+              (done-fn)
+              (is (alt!!
+                    c false
+                    (async/timeout 10000) true)))
+            (stop-fn)))
+        (testing "unacknowledged messages get resent"
+          (let [c       (chan)
+                stop-fn (su/receive-loop! creds @test-queue-url c {:auto-delete false
+                                                                   :visibility-timeout 5})]
+            (is (su/send-message creds @test-queue-url {:testing 7} {:format format}))
+            (let [{:keys [message done-fn]} (<!! c)]
+              (is (= {:testing 7} message))
+              (is (some? done-fn))
+              ;; don't call it, wait for the next one
+              (wait-for
+                #(alt!!
+                   c ([{:keys [message done-fn]} _]
+                      (= message {:testing 7}))
+                   (async/timeout 500) false)
+                :timeout 30))
+            (stop-fn)))))))
 
 (deftest send-message-test
-  (let [{:keys [endpoint] :as creds} (sqs-config)]
-    (testing "Success case"
-      (is (= [:id :body-md5]
-             (keys (su/send-message creds @test-queue-url {:testing 28})))))
+  (doseq [format [:json :transit]]
+    (with-queue
+      (let [{:keys [endpoint] :as creds} (sqs-config)]
+        (testing "Success case"
+          (is (= [:id :body-md5]
+                 (keys (su/send-message creds @test-queue-url {:testing 28} {:format format})))))
 
-    (testing "Fail case"
-      (is (thrown? clojure.lang.ExceptionInfo
-                   (su/send-message creds
-                                    (str endpoint "/queue/non-existing")
-                                    {:testing 1}))))))
+        (testing "Fail case"
+          (is (thrown? clojure.lang.ExceptionInfo
+                       (su/send-message creds
+                                        (str endpoint "/queue/non-existing")
+                                        {:testing 1}
+                                        {:format format}))))))))
 
 (deftest send-fifo-message-test
-  (testing "Fail case, missing :message-group-id"
-    (is (thrown? java.lang.AssertionError
-                 (su/send-fifo-message
-                   (sqs-config)
-                   @test-queue-url
-                   {:testing 1}
-                   {})))))
+  (doseq [format [:json :transit]]
+    (with-queue
+      (testing "Fail case, missing :message-group-id"
+        (is (thrown? java.lang.AssertionError
+                     (su/send-fifo-message
+                       (sqs-config)
+                       @test-queue-url
+                       {:testing 1}
+                       {:format format})))))))
 
 (deftest roundtrip-datetime-test
-  (let [coll {:data [1 2 3]
-              :timestamp (t/now)}
-        creds (sqs-config)]
-    (is (su/send-message creds @test-queue-url coll))
-    (is (= coll (su/receive-one! creds @test-queue-url)))))
+  (with-queue
+    (let [coll {:data [1 2 3]
+                :timestamp (t/now)}
+          creds (sqs-config)]
+      (is (su/send-message creds @test-queue-url coll))
+      (is (= coll (su/receive-one! creds @test-queue-url))))))
 
 (deftest terminate-receive-loop
-  (let [c (chan)
-        creds (sqs-config)]
-    (is (su/send-message creds @test-queue-url {:testing 4}))
-    (let [kill-fn (su/receive-loop! creds @test-queue-url c)]
-      (is (some? kill-fn))
-      (is (= {:testing 4} (:message (<!! c))))
-      ;; terminate the loop, close the channel
-      (kill-fn)
-      ;; send a message to the queue, which still exists
-      (su/send-message creds @test-queue-url {:testing 5})
-      ;; closed channel should return nil - TODO some other way to verify?
-      (is (nil? (<!! c))))))
+  (doseq [format [:json :transit]]
+    (with-queue
+      (let [c (chan)
+            creds (sqs-config)]
+        (is (su/send-message creds @test-queue-url {:testing 4} {:format format}))
+        (let [kill-fn (su/receive-loop! creds @test-queue-url c)]
+          (is (some? kill-fn))
+          (is (= {:testing 4} (:message (<!! c))))
+          ;; terminate the loop, close the channel
+          (kill-fn)
+          ;; send a message to the queue, which still exists
+          (su/send-message creds @test-queue-url {:testing 5} {:format format})
+          ;; closed channel should return nil - TODO some other way to verify?
+          (is (nil? (<!! c))))))))
 
 (deftest fink-nottle-error-handling-works
-  (letfn [(run-one-test [throwable]
-            (let [c              (chan)
-                  messages-chan1 (chan)
-                  creds          (sqs-config)]
-              (bond/with-stub! [[sqs.channeled/receive! (constantly messages-chan1)]]
-                (let [kill-fn (su/receive-loop! creds @test-queue-url c)]
-                  (try
-                    (is (fn? kill-fn))
-                    (>!! messages-chan1 {:body :hello})
-                    (is (= :hello (:message (<!! c))))
-                    (is (= 1 (-> sqs.channeled/receive! bond/calls count)))
-                    ;; set up the next channel to return when receive is called
-                    (let [messages-chan2 (chan)]
-                      (bond/with-stub! [[sqs.channeled/receive! (constantly messages-chan2)]]
-                        ;; ensure it hasn't been called yet
-                        (is (= 0 (-> sqs.channeled/receive! bond/calls count)))
-                        ;; fire off an error
-                        (>!! messages-chan1 (ex-info "test message" {}))
-                        ;; receive should be called again
-                        (wait-for #(= 1 (-> sqs.channeled/receive! bond/calls count)))
-                        ;; first channel should be closed
-                        (is (clojure.core.async.impl.protocols/closed? messages-chan1))
-                        ;; second channel should be ok
-                        (is (not (clojure.core.async.impl.protocols/closed? messages-chan2)))
-                        ;; out-chan should be ok
-                        (is (not (clojure.core.async.impl.protocols/closed? c)))
-                        ;; everything should still work
-                        (>!! messages-chan2 {:body :still-works})
-                        (is (= :still-works (:message (<!! c))))
-
-                        ;; terminate the loop
-                        (let [stats (kill-fn)]
-                          (is (= 1 (:restart-count stats)))
-                          (is (instance? org.joda.time.DateTime (:restarted-at stats)))
-                          (is (= 3 (:count stats)))
-                          ;; everything should be closed
+  (with-queue
+    (letfn [(run-one-test [throwable]
+              (let [c              (chan)
+                    messages-chan1 (chan)
+                    creds          (sqs-config)]
+                (bond/with-stub! [[sqs.channeled/receive! (constantly messages-chan1)]]
+                  (let [kill-fn (su/receive-loop! creds @test-queue-url c)]
+                    (try
+                      (is (fn? kill-fn))
+                      (>!! messages-chan1 {:body :hello})
+                      (is (= :hello (:message (<!! c))))
+                      (is (= 1 (-> sqs.channeled/receive! bond/calls count)))
+                      ;; set up the next channel to return when receive is called
+                      (let [messages-chan2 (chan)]
+                        (bond/with-stub! [[sqs.channeled/receive! (constantly messages-chan2)]]
+                          ;; ensure it hasn't been called yet
+                          (is (= 0 (-> sqs.channeled/receive! bond/calls count)))
+                          ;; fire off an error
+                          (>!! messages-chan1 (ex-info "test message" {}))
+                          ;; receive should be called again
+                          (wait-for #(= 1 (-> sqs.channeled/receive! bond/calls count)))
+                          ;; first channel should be closed
                           (is (clojure.core.async.impl.protocols/closed? messages-chan1))
-                          (is (clojure.core.async.impl.protocols/closed? messages-chan2))
-                          (is (clojure.core.async.impl.protocols/closed? c)))))
-                    (catch Throwable t
-                      (kill-fn)
-                      (throw t)))))))]
+                          ;; second channel should be ok
+                          (is (not (clojure.core.async.impl.protocols/closed? messages-chan2)))
+                          ;; out-chan should be ok
+                          (is (not (clojure.core.async.impl.protocols/closed? c)))
+                          ;; everything should still work
+                          (>!! messages-chan2 {:body :still-works})
+                          (is (= :still-works (:message (<!! c))))
 
-    (testing "for ex-info wrapped errors"
-      (run-one-test (ex-info "test message" {})))
+                          ;; terminate the loop
+                          (let [stats (kill-fn)]
+                            (is (= 1 (:restart-count stats)))
+                            (is (instance? org.joda.time.DateTime (:restarted-at stats)))
+                            (is (= 3 (:count stats)))
+                            ;; everything should be closed
+                            (is (clojure.core.async.impl.protocols/closed? messages-chan1))
+                            (is (clojure.core.async.impl.protocols/closed? messages-chan2))
+                            (is (clojure.core.async.impl.protocols/closed? c)))))
+                      (catch Throwable t
+                        (kill-fn)
+                        (throw t)))))))]
 
-    (testing "for unwrapped java exceptions"
-      (run-one-test (NoSuchMethodError. "test message"))
-      (run-one-test (Exception. "test message")))))
+      (testing "for ex-info wrapped errors"
+        (run-one-test (ex-info "test message" {})))
+
+      (testing "for unwrapped java exceptions"
+        (run-one-test (NoSuchMethodError. "test message"))
+        (run-one-test (Exception. "test message"))))))
--- test/sqs_utils/integration_test.clj
+++ test/sqs_utils/integration_test.clj
@@ -9,8 +9,6 @@
              [sqs-utils.test-utils :as sqs-test-utils])
   (:import [java.util UUID]))
 
-(defn uuid [] (UUID/randomUUID))
-
 (defn sqs-config []
   {:access-key (env/env :integration-aws-access-key)
    :secret-key (env/env :integration-aws-secret-key)
@@ -18,68 +16,10 @@
    :region     "us-west-2"})
 
 (def standard-queue-url
-  (env/env :standard-integration-test-queue-url))
+   (env/env :standard-integration-test-queue-url))
 
 (def fifo-queue-url
   (env/env :fifo-integration-test-queue-url))
-
-(deftest ^:integration receive-one-test
-  (testing "standard queue"
-    (let [coll {:testing (str (uuid))}]
-      (is (sqs-utils/send-message (sqs-config) standard-queue-url coll))
-      (is (= coll (sqs-utils/receive-one! (sqs-config) standard-queue-url)))))
-  (testing "fifo queue"
-    (let [coll {:testing (str (uuid))}]
-      (is (sqs-utils/send-fifo-message (sqs-config) fifo-queue-url coll {:message-group-id 3}))
-      (is (= coll (sqs-utils/receive-one! (sqs-config) fifo-queue-url))))))
-
-(deftest ^:integration send-and-receive-message-test
-  (testing "standard queue"
-    (let [c (chan)
-          msg1 {:testing (str (uuid))}
-          msg2 {:testing (str (uuid))}]
-      (is (sqs-utils/send-message (sqs-config) standard-queue-url msg1))
-      (let [stop-fn (sqs-utils/receive-loop! (sqs-config) standard-queue-url c)]
-        (is (= msg1 (:message (<!! c))))
-        (is (sqs-utils/send-message (sqs-config) standard-queue-url msg2))
-        (is (= msg2 (:message (<!! c))))
-        (let [stats (stop-fn)]
-          (is (empty? (:restarts stats))))))))
-
-(deftest ^:integration send-and-receive-fifo-message-test
-  (testing "fifo queue"
-    (let [c (chan)
-          msg1 {:testing (str (uuid))}
-          msg2 {:testing (str (uuid))}]
-      (is (sqs-utils/send-fifo-message (sqs-config) fifo-queue-url msg1 {:message-group-id 2}))
-      (let [stop-fn (sqs-utils/receive-loop! (sqs-config) fifo-queue-url c)]
-        (is (= msg1 (:message (<!! c))))
-        (is (sqs-utils/send-fifo-message (sqs-config) fifo-queue-url msg2 {:message-group-id 1}))
-        (is (= msg2 (:message (<!! c))))
-        (let [stats (stop-fn)]
-          (is (empty? (:restarts stats)))))))
-
-  (testing  "fifo queue with deduplication"
-    (let  [c    (chan)
-           msg1 {:testing 1}
-           msg2 {:testing 2}
-           msg3 {:testing 3}]
-      (is  (sqs-utils/send-fifo-message  (sqs-config) fifo-queue-url msg1 {:message-group-id 1 :deduplication-id 1}))
-      (is  (sqs-utils/send-fifo-message  (sqs-config) fifo-queue-url msg2 {:message-group-id 1 :deduplication-id 1}))
-      (is  (sqs-utils/send-fifo-message  (sqs-config) fifo-queue-url msg3 {:message-group-id 1 :deduplication-id 2}))
-      (let  [stop-fn  (sqs-utils/receive-loop!  (sqs-config) fifo-queue-url c)]
-        (is  (= msg1 (:message (<!! c))))
-        ;; second message doesn't exist because it has been de-duped
-        (is  (= msg3 (:message (<!! c))))
-        (let  [stats  (stop-fn)]
-          (is  (empty?  (:restarts stats))))))))
-
-(deftest ^:integration roundtrip-datetime-test
-  (let [coll {:data [1 2 3]
-              :uuid (str (uuid))
-              :timestamp (t/now)}]
-    (is (sqs-utils/send-message (sqs-config) standard-queue-url coll))
-    (is (= coll (sqs-utils/receive-one! (sqs-config) standard-queue-url)))))
 
 (defn wrap-purge-queues
   [f]
@@ -91,4 +31,85 @@
     (sqs-test-utils/purge-queue! config standard-queue-url)
     (sqs-test-utils/purge-queue! config fifo-queue-url)))
 
-(use-fixtures :once wrap-purge-queues)
+(defmacro with-test-queues [& body]
+  `(wrap-purge-queues
+     (fn [] ~@body)))
+
+(defn uuid [] (UUID/randomUUID))
+
+(deftest ^:integration receive-one-test
+  (doseq [format [:json :transit]]
+    (with-test-queues
+      (testing "standard queue"
+        (let [coll {:testing (str (uuid))}]
+          (is (sqs-utils/send-message (sqs-config) standard-queue-url coll {:format format}))
+          (is (= coll (sqs-utils/receive-one! (sqs-config) standard-queue-url)))))
+      (testing "fifo queue"
+        (let [coll {:testing (str (uuid))}]
+          (is (sqs-utils/send-fifo-message (sqs-config) fifo-queue-url coll {:message-group-id 3
+                                                                             :format format}))
+          (is (= coll (sqs-utils/receive-one! (sqs-config) fifo-queue-url))))))))
+
+(deftest ^:integration send-and-receive-message-test
+  (doseq [format [:json :transit]]
+    (with-test-queues
+      (testing "standard queue"
+        (let [c (chan)
+              msg1 {:testing (str (uuid))}
+              msg2 {:testing (str (uuid))}]
+          (is (sqs-utils/send-message (sqs-config) standard-queue-url msg1 {:format format}))
+          (let [stop-fn (sqs-utils/receive-loop! (sqs-config) standard-queue-url c)]
+            (is (= msg1 (:message (<!! c))))
+            (is (sqs-utils/send-message (sqs-config) standard-queue-url msg2 {:format format}))
+            (is (= msg2 (:message (<!! c))))
+            (let [stats (stop-fn)]
+              (is (empty? (:restarts stats))))))))))
+
+(deftest ^:integration send-and-receive-fifo-message-test
+  (doseq [format [:json :transit]]
+    (with-test-queues
+      (testing "fifo queue"
+        (let [c (chan)
+              msg1 {:testing (str (uuid))}
+              msg2 {:testing (str (uuid))}]
+          (is (sqs-utils/send-fifo-message (sqs-config) fifo-queue-url msg1 {:message-group-id 2
+                                                                             :format format}))
+          (let [stop-fn (sqs-utils/receive-loop! (sqs-config) fifo-queue-url c)]
+            (is (= msg1 (:message (<!! c))))
+            (is (sqs-utils/send-fifo-message (sqs-config) fifo-queue-url msg2 {:message-group-id 1
+                                                                               :format format}))
+            (is (= msg2 (:message (<!! c))))
+            (let [stats (stop-fn)]
+              (is (empty? (:restarts stats)))))))
+
+      (testing  "fifo queue with deduplication"
+        (let  [c    (chan)
+               msg1 {:testing 1}
+               msg2 {:testing 2}
+               msg3 {:testing 3}]
+          (is  (sqs-utils/send-fifo-message  (sqs-config) fifo-queue-url msg1 {:message-group-id 1
+                                                                               :deduplication-id 1
+                                                                               :format format}))
+          (is  (sqs-utils/send-fifo-message  (sqs-config) fifo-queue-url msg2 {:message-group-id 1
+                                                                               :deduplication-id 1
+                                                                               :format format}))
+          (is  (sqs-utils/send-fifo-message  (sqs-config) fifo-queue-url msg3 {:message-group-id 1
+                                                                               :deduplication-id 2
+                                                                               :format format}))
+          (let  [stop-fn  (sqs-utils/receive-loop!  (sqs-config) fifo-queue-url c)]
+            (is  (= msg1 (:message (<!! c))))
+            ;; second message doesn't exist because it has been de-duped
+            (is  (= msg3 (:message (<!! c))))
+            (let  [stats  (stop-fn)]
+              (is  (empty?  (:restarts stats))))))))))
+
+(deftest ^:integration roundtrip-datetime-test
+  ;; this one is only relevant for transit, since JSON does not have a date/time
+  ;; related data type, just strings.
+  (with-test-queues
+    (let [coll {:data [1 2 3]
+                :uuid (str (uuid))
+                :timestamp (t/now)}]
+      (is (sqs-utils/send-message (sqs-config) standard-queue-url coll))
+      (is (= coll (sqs-utils/receive-one! (sqs-config) standard-queue-url))))))
+
