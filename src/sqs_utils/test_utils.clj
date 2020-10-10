ddc95b16c5a92723168273b6c30dfb643136ca34
Paul Lam <paul@quantisan.com>
2020-10-10 09:18:29 +0900

    update test-utils

--- src/sqs_utils/test_utils.clj
+++ src/sqs_utils/test_utils.clj
@@ -1,26 +1,29 @@
 (ns sqs-utils.test-utils
   "Utilities and helpers for dev and test use"
   (:require [clojure.tools.logging :as log]
-            [fink-nottle.sqs :as sqs]
-            [clojure.core.async :refer [<!!]])
+            [sqs-utils.impl :as impl]
+            [clj-sqs-extended.aws.sqs :as sqs])
   (:import [java.util UUID]))
+
+(defn random-queue-name []
+  (str "test-queue-" (UUID/randomUUID)))
 
 (defn create-queue!
   ([sqs-config queue-name]
-   (let [url (<!! (sqs/create-queue! sqs-config queue-name))]
+   (let [url (sqs/create-standard-queue! (impl/sqs-ext-client sqs-config) queue-name)]
      (log/info "Created queue at" url)
      url))
   ([sqs-config]
-   (create-queue! sqs-config (str "test-queue-" (UUID/randomUUID)))))
+   (create-queue! sqs-config (random-queue-name))))
 
 (defn delete-queue! [sqs-config queue-url]
   (log/info "Deleting queue at" queue-url)
-  (<!! (sqs/delete-queue! sqs-config queue-url)))
+  (sqs/delete-queue! (impl/sqs-ext-client sqs-config) queue-url))
 
 (defn purge-queue!
   [sqs-config queue-url]
   (log/info "Purging queue at" queue-url)
-  (<!! (sqs/purge-queue! sqs-config queue-url)))
+  (sqs/purge-queue! (impl/sqs-ext-client sqs-config) queue-url))
 
 (defn init-queue
   "This should NOT be invoked in production.
@@ -38,4 +41,6 @@
     {:url                (create-queue! sqs-config (name queue))
      :threads            n-threads
      :consumers          n-consumers
+     ;; TODO remove visibility-timeout
      :visibility-timeout timeout}}))
+
