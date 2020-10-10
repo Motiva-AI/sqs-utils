f70202420265a0a22c8ffb4a14b5cb6b14312c7c
Paul Lam <paul@quantisan.com>
2020-10-10 15:04:39 +0900

    integration tests remove unnecessary format check

--- test/sqs_utils/integration_test.clj
+++ test/sqs_utils/integration_test.clj
@@ -47,55 +47,48 @@
         (is (= coll (sqs-utils/receive-one! (sqs-config) fifo-queue-url)))))))
 
 (deftest ^:integration send-and-receive-message-test
-  (doseq [format [:json :transit]]
-    (testing "standard queue"
-      (let [c (chan)
-            msg1 {:testing (str (uuid))}
-            msg2 {:testing (str (uuid))}]
-        (is (sqs-utils/send-message (sqs-config) standard-queue-url msg1 {:format format}))
-        (let [stop-fn (sqs-utils/receive-loop! (sqs-config) standard-queue-url c)]
-          (is (= msg1 (:message (<!! c))))
-          (is (sqs-utils/send-message (sqs-config) standard-queue-url msg2 {:format format}))
-          (is (= msg2 (:message (<!! c))))
-          (let [stats (stop-fn)]
-            (is (empty? (:restarts stats)))))))))
+  (testing "standard queue"
+    (let [c (chan)
+          msg1 {:testing (str (uuid))}
+          msg2 {:testing (str (uuid))}]
+      (is (sqs-utils/send-message (sqs-config) standard-queue-url msg1))
+      (let [stop-fn (sqs-utils/receive-loop! (sqs-config) standard-queue-url c)]
+        (is (= msg1 (:message (<!! c))))
+        (is (sqs-utils/send-message (sqs-config) standard-queue-url msg2))
+        (is (= msg2 (:message (<!! c))))
+        (let [stats (stop-fn)]
+          (is (empty? (:restarts stats))))))))
 
 (deftest ^:integration send-and-receive-fifo-message-test
-  (doseq [format [:json :transit]]
-    (testing "fifo queue"
-      (let [c (chan)
-            msg1 {:testing (str (uuid))}
-            msg2 {:testing (str (uuid))}]
-        (is (sqs-utils/send-fifo-message (sqs-config) fifo-queue-url msg1 {:message-group-id 2
-                                                                           :format format}))
-        (let [stop-fn (sqs-utils/receive-loop! (sqs-config) fifo-queue-url c)]
-          (is (= msg1 (:message (<!! c))))
-          (is (sqs-utils/send-fifo-message (sqs-config) fifo-queue-url msg2 {:message-group-id 1
-                                                                             :format format}))
-          (is (= msg2 (:message (<!! c))))
-          (let [stats (stop-fn)]
-            (is (empty? (:restarts stats)))))))
+  (testing "fifo queue"
+    (let [c (chan)
+          msg1 {:testing (str (uuid))}
+          msg2 {:testing (str (uuid))}]
+      (is (sqs-utils/send-fifo-message (sqs-config) fifo-queue-url msg1 {:message-group-id 2}))
+      (let [stop-fn (sqs-utils/receive-loop! (sqs-config) fifo-queue-url c)]
+        (is (= msg1 (:message (<!! c))))
+        (is (sqs-utils/send-fifo-message (sqs-config) fifo-queue-url msg2 {:message-group-id 1}))
+        (is (= msg2 (:message (<!! c))))
+        (let [stats (stop-fn)]
+          (is (empty? (:restarts stats)))))))
 
-    (testing  "fifo queue with deduplication"
-      (let  [c    (chan)
-             msg1 {:testing 1}
-             msg2 {:testing 2}
-             msg3 {:testing 3}]
-        (is  (sqs-utils/send-fifo-message  (sqs-config) fifo-queue-url msg1 {:message-group-id 1
-                                                                             :deduplication-id 1
-                                                                             :format format}))
-        (is  (sqs-utils/send-fifo-message  (sqs-config) fifo-queue-url msg2 {:message-group-id 1
-                                                                             :deduplication-id 1
-                                                                             :format format}))
-        (is  (sqs-utils/send-fifo-message  (sqs-config) fifo-queue-url msg3 {:message-group-id 1
-                                                                             :deduplication-id 2
-                                                                             :format format}))
-        (let  [stop-fn  (sqs-utils/receive-loop!  (sqs-config) fifo-queue-url c)]
-          (is  (= msg1 (:message (<!! c))))
-          ;; second message doesn't exist because it has been de-duped
-          (is  (= msg3 (:message (<!! c))))
-          (let  [stats  (stop-fn)]
-            (is  (empty?  (:restarts stats)))))))))
+  (testing  "fifo queue with deduplication"
+    (let  [c    (chan)
+           msg1 {:testing 1}
+           msg2 {:testing 2}
+           msg3 {:testing 3}]
+      (is  (sqs-utils/send-fifo-message  (sqs-config) fifo-queue-url msg1 {:message-group-id 1
+                                                                           :deduplication-id 1}))
+      (is  (sqs-utils/send-fifo-message  (sqs-config) fifo-queue-url msg2 {:message-group-id 1
+                                                                           :deduplication-id 1}))
+      (is  (sqs-utils/send-fifo-message  (sqs-config) fifo-queue-url msg3 {:message-group-id 1
+                                                                           :deduplication-id 2}))
+      (let  [stop-fn  (sqs-utils/receive-loop!  (sqs-config) fifo-queue-url c)]
+        (is  (= msg1 (:message (<!! c))))
+        ;; second message doesn't exist because it has been de-duped
+        (is  (= msg3 (:message (<!! c))))
+        (let  [stats  (stop-fn)]
+          (is  (empty?  (:restarts stats))))))))
 
 (deftest ^:integration roundtrip-datetime-test
   ;; this one is only relevant for transit, since JSON does not have a date/time
